9c56b509bc4410523352e3adcb0abf1397fb00b7
sydneycode <sstaggs12@gmail.com>
2018-08-16 18:25:02 -0700

    set the game up to randomly pick the next word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,121 +4,148 @@ console.log("here");
 // The word bank we'll be using for this game
 var wordBank = ["APPLE", "BANANA", "ORANGE"];
 
-// Representation of Game as an object
-// var game = {
-//     currentWordIndex: 0,
-//     currentWord: "",
-//     currentRepresentationOfWord: "",
-//     wins: 0,
-//     guessesRemaining: 12,
-//     lettersGuessed: [],
-//     lettersToBeGuessed: []
-// };
+//Representation of Game as an object
+var game = {
+    wordIndex: 0,
+    word: "",
+    repOfWord: "",
+    wins: 0,
+    guessesRemaining: 12,
+    lettersGuessed: [],
+    lettersToBeGuessed: []
+};
 
-var currentWordIndex= 0;
-var currentWord= "";
-var currentRepresentationOfWord= "";
-var wins= 0;
-var guessesRemaining= 12;
-var lettersGuessed= [];
-var lettersToBeGuessed= [];
+var wordIndex = 0;
+var word = "";
+var repOfWord = "";
+var wins = 0;
+var guessesRemaining = 12;
+var lettersGuessed = [];
+var lettersToBeGuessed = [];
+var gameOver = false;
 
-pickANewQuestion();
-setUpPageWithNewQuestion(currentWordIndex);
+// Initialize the page with the first word to be guessed
+setUpPageWithNewWord();
+updatePage();
 
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
     var userKey = event.key.toUpperCase();
     console.log(userKey);
 
-    compareLetterToListOfLetters(userKey);
-    guessesRemaining--;
-    lettersGuessed.push(userKey);
-    
-    if (lettersToBeGuessed.length === 0) {
-        wins++;
-        lettersGuessed = [];
-        guessesRemaining = 12;
-    }
-
-    if (guessesRemaining <= 0 || lettersToBeGuessed.length === 0) {
-        pickANewQuestion();
-        setUpPageWithNewQuestion(currentWordIndex);
+    // Detect if the user pressed a key not representing a letter
+    if (event.keyCode < 65 || event.keyCode > 90) {
+        alert("Please press a letter key to play!");
     }
     else {
-        updatePage();
+        // If the user pressed a key not already in the list of letters guessed, 
+        // then proceed with comparing the key pressed to the list of letters to 
+        // be guessed and updating the page as necessary
+        if (!isLetterInListOfLetters(userKey)) {
+            compareLetterToListOfLetters(userKey);
+            guessesRemaining--;
+            lettersGuessed.push(userKey);
+
+            // If there are no more letters to be guessed, then you have
+            // won the game for this word!
+            if (lettersToBeGuessed.length === 0) {
+                wins++;
+                lettersGuessed = [];
+                guessesRemaining = 12;
+                setUpPageWithNewWord();
+            }
+            else if (guessesRemaining === 0) {
+                // pick a new question, but don't increment the number of wins
+                lettersGuessed = [];
+                guessesRemaining = 12;
+                setUpPageWithNewWord();
+            }
+            updatePage();
+
+        }
     }
-
 }
 
-function pickANewQuestion() {
-    //pick a random number
-    //set current word index equal to the random number
-    //for now, current word index is 0
-    currentWordIndex = 0;
+function setUpPageWithNewWord() {
+    // Randomly select the index of the new word 
+    wordIndex = Math.floor(Math.random() * wordBank.length);
+    console.log(wordIndex);
 
-    currentWord = wordBank[currentWordIndex];
-    lettersToBeGuessed = convertWordToListOfLetters(currentWord);
-    currentRepresentationOfWord = createRepresentationOfWord(currentWord);
+    word = wordBank[wordIndex];
+    convertWordToListOfLetters();
+    createRepresentationOfWord();
+    console.log(word);
 }
 
+// Update the page with the correct information
 function updatePage() {
     document.querySelector("#number-of-wins").innerHTML = wins;
-    document.querySelector("#word-in-progress").innerHTML = currentRepresentationOfWord;
+    document.querySelector("#word-in-progress").innerHTML = repOfWord;
     document.querySelector("#number-of-guesses-remaining").innerHTML = guessesRemaining;
     document.querySelector("#letters-already-guessed").innerHTML = lettersGuessed;
 }
 
-function convertWordToListOfLetters(word) {
+function convertWordToListOfLetters() {
     var listOfLetters = [];
-    for (var i = 0; i < word.length; i++)
-    {
+    for (var i = 0; i < word.length; i++) {
         // if the current word is not yet in the list of letters...
-        if (listOfLetters.indexOf(word[i]) === -1)
-        {
+        if (listOfLetters.indexOf(word[i]) === -1) {
             // add the current letter to the list of letters in the word
             listOfLetters.push(word[i]);
         }
     }
-    console.log(listOfLetters);
-    return listOfLetters;
+    lettersToBeGuessed = listOfLetters;
+    console.log(lettersToBeGuessed);
+}
+
+function isLetterInListOfLetters(letter) {
+    for (var i = 0; i < lettersGuessed.length; i++) {
+        if (letter === lettersGuessed[i]) {
+            return true;
+        }
+    }
 }
 
 function compareLetterToListOfLetters(letter) {
     for (var i = 0; i < lettersToBeGuessed.length; i++) {
         // if the current letter is in the list of letters...
-        if (letter === lettersToBeGuessed[i])
-        {
+        if (letter === lettersToBeGuessed[i]) {
             // delete the current letter from the list of letters to be guessed
             // (at position i, remove 1 item from the list)
             lettersToBeGuessed.splice(i, 1);
             console.log(lettersToBeGuessed);
 
             // update current representation of the word
-            updateRepresentationOfWord(currentWord);
+            updateRepresentationOfWord(letter);
         }
     }
+
 }
 
-// Create a representation of the input word to display,
+// Create a representation of the current word to display,
 // where each letter is replaced with an underscore, i.e.
 // "APPLE" is represented by "_____"
-function createRepresentationOfWord(word) {
+function createRepresentationOfWord() {
     var rep = "";
     for (var i = 0; i < word.length; i++) {
         rep += "_";
     }
-    return rep;
+    repOfWord = rep;
+    console.log(repOfWord);
 }
 
-function updateRepresentationOfWord(word) {
+// Go through the current representation of the current word
+// and, for each character in the current word that matches the 
+// given letter, replace each underscore with the given letter
+function updateRepresentationOfWord(letter) {
     var updatedRepOfWord = "";
-    for (var i = 0; i < currentWord.length; i++) {
-        if (letter === currentWord.charAt(i)) {
+    for (var i = 0; i < word.length; i++) {
+        if (letter === word.charAt(i)) {
             updatedRepOfWord += letter;
         }
         else {
-            updatedRepOfWord += currentRepresentationOfWord[i];
+            updatedRepOfWord += repOfWord[i];
         }
     }
-    currentRepresentationOfWord = updatedRepOfWord;
+    repOfWord = updatedRepOfWord;
+    console.log(repOfWord);
 }
